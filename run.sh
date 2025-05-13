083dd3faae6aab11880d982e71473149db175db1
soumitradev <soumitra.shewale.dev@gmail.com>
2025-05-13 18:18:46 +0530

    build: add run.sh script to run code

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p out
+apptainer exec --env LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda-12/compat:/legion/bindings/regent --env CPATH=\$CPATH:/usr/local/cuda-12/targets/x86_64-linux/include/ --bind ./src:/src --bind ./data:/data --bind ./build:/build ../regent_env/v24.06.0/v24.06.0.sif /build/meshfree_solver_test.out -ll:csize 77G -ll:cpu 15
